47fbf5e0b6b9316f2a3150410518a30f51129a0f
Chris Lyth <cjlyth@gmail.com>
2016-09-25 02:30:33 -0500

    Got the app working!

--- src/serial_read.py
+++ src/serial_read.py
@@ -18,18 +18,36 @@ counter=0
 logsUrl='https://sousvide.lyth.io/api/logs'
 headers = {'Content-Type': 'application/json'}
 
+ser.write('0')
 
 def getConfig():
     resp = requests.get("https://sousvide.lyth.io/api/configuration")
-    resp.raise_for_status()
-    return resp.json();
+    if resp.status_code == 200 :
+        return resp.json();
+    else :
+        return {'running': False}
+
+def sendLog():
+    payload = json.dumps({'temperature': ser.readline()})
+    resp = requests.post(logsUrl, headers = headers, data = payload)
+    if resp.status_code == 200 :
+        return resp.json();
+    else :
+        return {'temperature':0, 'timestamp':0}
 
 while True:
     config = getConfig()
     if config['running'] :
-        print('is running: {}').format(config['running'])
-        payload = json.dumps({'temperature': ser.readline()})
-        r = requests.post(logsUrl, headers = headers, data = payload)
-        logentry = r.json()
-        print('{} - posted - {} - {}').format(r.status_code, logentry['temperature'], logentry['timestamp'])
+        minTemp = config['temperature'] - 5
+        maxTemp = config['temperature'] + 5
+        logEntry = sendLog()
+        print('current temp: {}, desired range: {} - {}').format(logEntry['temperature'],minTemp,maxTemp)
+        if logEntry['temperature'] > 0:
+            if logEntry['temperature'] < maxTemp:
+                ser.write('1')
+            else:
+                ser.write('0')
+            # print('is running: {}').format(config['running'])
+    else:
+        ser.write('0')
     time.sleep(10)
